4614630ae4873e2083f595e9e104ac14da3f81dc
brita lindholm <brita.lindholm1978@gmail.com>
2018-11-19 18:06:40 +0200

    Opetellaan gitin käyttöä

---
+++ OmaHarjoitus/Controllers/HenkiloController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+
+namespace OmaHarjoitus.Controllers
+{
+    public class HenkiloController : Controller
+    {
+        private ProjektitEntities db = new ProjektitEntities();
+
+        // GET: Henkilo
+        public ActionResult Index()
+        {
+            return View(db.Henkilo.ToList());
+        }
+    }
+}
---
+++ OmaHarjoitus/Models/TestiModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OmaHarjoitus.Models
+{
+    public class TestiModel
+    {
+        public string Etunimi { get; set; }
+        public string Sukunimi { get; set; }
+        public string Osoite { get; set; }
+        public int Esimies { get; set; }
+        public int HenkiloID { get; set; }
+
+        public string Nimi { get; set; }
+        public string Status { get; set; }
+
+        public int ProjektiID { get; set; }
+
+        public int TuntiID { get; set; }
+        public DateTime PVM { get; set; }
+
+        public virtual Henkilo Henkilo { get; set;}
+        public virtual Projekti Projekti { get; set; }
+        public virtual Tunti Tunti { get; set; }
+
+        
+
+    }
+}
